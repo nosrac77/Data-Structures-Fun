34c2b6e8666154163fb27e85c08018d19f81675b
Carson Newton <carson.newton@outlook.com>
2018-03-27 16:23:09 -0700

    added comments to add_node method

--- python_data_structures/singly_linked_list.py
+++ python_data_structures/singly_linked_list.py
@@ -23,8 +23,20 @@ class SinglyLinkedList(object):
     def add_node(self, data):
         """Method to add new Node to Singly Linked List."""
 
-        if self.head:
-            new_next_node = self.head
-            self.head = Node(data, new_next_node)
-        else:
-            self.head = Node(data)
+        # When we want to add a new Node to our Singly Linked List, all we have
+        # to do is 1) create a variable that contains the current list's head
+        # Node and 2) reassign the list's head Node to become a new instance
+        # of our Node class.
+
+        new_next_node = self.head
+
+        # 1) On line 31 we create a new variable called new_next_node
+        # and assign it's value to the list's current self.head. We'll then use
+        # this variable below on step 2.
+
+        self.head = Node(data, new_next_node)
+
+        # 2) On line 37 we reassign the list's head Node to become the newly
+        # added Node. It's data will be the data passed in as the
+        # argument for our method and it's next_node will become what the old
+        # head Node used to be.

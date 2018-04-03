6161f620cd88b624efd05160d68370647751d4ef
Carson Newton <carson.newton@outlook.com>
2018-04-03 16:22:17 -0700

    created initial scaffold for the doubly linked list implementation in Python

--- python_data_structures/doubly_linked_list.py
+++ python_data_structures/doubly_linked_list.py
@@ -1,1 +1,20 @@
 """Module to implement Doubly Linked List in Python."""
+
+
+class Node(object):
+    """Class to create instance of Node."""
+
+    def __init__(self, data, next_node, prev_node):
+        """Create new instance of Node class object."""
+        self.data = data
+        self.next_node = next_node
+        self.prev_node = prev_node
+
+
+class DoublyLinkedList(object):
+    """Class to create instance of Doubly Linked List."""
+
+    def __init__(self, head, tail):
+        """Create new instance of DoublyLinkedList class object."""
+        self.head = head
+        self.tail = tail

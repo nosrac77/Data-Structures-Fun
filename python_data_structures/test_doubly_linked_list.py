11233de9c4b8ce5eeba88622b1cfa25d593a650d
Carson Newton <carson.newton@outlook.com>
2018-04-04 16:00:53 -0700

    added several tests for delete_node method, found one bug, so reworking method now

--- python_data_structures/test_doubly_linked_list.py
+++ python_data_structures/test_doubly_linked_list.py
@@ -67,3 +67,62 @@ def test_add_node_method_properly_assigns_prev_node_pointer():
     dll.add_node(20)
 
     assert dll.tail.prev_node.data == 20
+
+
+def test_delete_node_method_returns_string_if_deleted_node_is_head():
+    """Test that the delete_node method of the DoublyLinkedList class returns a
+    specific string if Node to be deleted is the head of the list."""
+
+    dll = DoublyLinkedList()
+    dll.add_node(1)
+    dll.add_node(2)
+
+    assert dll.delete_node(2) == 'Node has been deleted.'
+
+
+def test_delete_node_method_returns_string_if_deleted_node_is_found():
+    """Test that the delete_node method of the DoublyLinkedList class returns a
+    specific string if Node to be deleted is found and removed."""
+
+    dll = DoublyLinkedList()
+    dll.add_node(0)
+    dll.add_node(5)
+    dll.add_node(10)
+
+    assert dll.delete_node(5) == 'Node has been deleted.'
+
+
+def test_delete_node_method_returns_string_if_deleted_node_is_tail():
+    """Test that the delete_node method of the DoublyLinkedList class returns a
+    specific string if Node to be deleted is the tail of the list."""
+
+    dll = DoublyLinkedList()
+    dll.add_node(3)
+    dll.add_node(6)
+
+    assert dll.delete_node(3) == 'Node has been deleted.'
+
+
+def test_delete_method_reassigns_head_and_tail_to_be_none_if_list_len_one():
+    """Test that the delete_node method of the DoublyLinkedList class assigns
+    the head and tail of the list to None if the list only has one Node."""
+
+    dll = DoublyLinkedList()
+    dll.add_node(1)
+
+    dll.delete_node(1)
+    assert dll.head is None and dll.tail is None
+
+
+def test_delete_node_method_removes_node_with_prev_and_next_pointers():
+    """Test that the delete_node method of the DoublyLinkedList class
+    successfully removes a Node with prev_node and next_node pointers actively
+    pointing to other Nodes."""
+
+    dll = DoublyLinkedList()
+
+    for i in range(3):
+        dll.add_node(i)
+
+    dll.delete_node(2)
+    assert dll.head.next_node.data != 2 and dll.tail.prev_node.data != 2

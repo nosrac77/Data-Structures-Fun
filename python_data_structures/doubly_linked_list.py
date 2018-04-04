95a77072005f9004fa22dd90a61eaf88c306c3e6
Carson Newton <carson.newton@outlook.com>
2018-04-03 19:09:06 -0700

    wrote add_node method of doubly linked list and added one test

--- python_data_structures/doubly_linked_list.py
+++ python_data_structures/doubly_linked_list.py
@@ -4,7 +4,7 @@
 class Node(object):
     """Class to create instance of Node."""
 
-    def __init__(self, data, next_node, prev_node):
+    def __init__(self, data, next_node=None, prev_node=None):
         """Create new instance of Node class object."""
         self.data = data
         self.next_node = next_node
@@ -14,7 +14,21 @@ class Node(object):
 class DoublyLinkedList(object):
     """Class to create instance of Doubly Linked List."""
 
-    def __init__(self, head, tail):
+    def __init__(self):
         """Create new instance of DoublyLinkedList class object."""
-        self.head = head
-        self.tail = tail
+        self.head = None
+        self.tail = None
+
+    def add_node(self, data):
+        """Method to add new Node to Doubly Linked List."""
+
+        new_next_node = self.head
+        self.head = Node(data, new_next_node)
+
+        if not self.head.next_node:
+
+            self.tail = self.head
+
+        else:
+
+            new_next_node.prev_node = self.head
--- python_data_structures/test_doubly_linked_list.py
+++ python_data_structures/test_doubly_linked_list.py
@@ -1,1 +1,13 @@
 """Module containing pytest tests for Doubly Linked List."""
+
+from doubly_linked_list import DoublyLinkedList, Node
+
+
+def test_add_node_method_adds_instance_of_node_to_list():
+    """Test that the add_node method of the DoublyLinkedList class adds an
+    instance of the Node class to the list."""
+
+    dll = DoublyLinkedList()
+    dll.add_node(1)
+
+    assert isinstance(dll.head, Node)

cd76c840cfc40cd0dce1936de96edf57764f9b61
nosrac77 <carson.newton@outlook.com>
2018-08-01 17:03:57 -0700

    added initialization method to Queue data structure in queue.py

--- python_data_structures/queue.py
+++ python_data_structures/queue.py
@@ -1,5 +1,13 @@
 """Module to implement Queue in Python."""
+from doubly_linked_list.py import DoublyLinkedList 
 
 
 class Queue(object):
     """Class to create instance of Queue."""
+
+    def __init__(self):
+        """Create new instance of Queue class object."""
+        self.queue = DoublyLinkedList()
+
+    #
+
